75750e42e3d1b0074f3c91341ccdd80d0c003bba
Vincent <vincentangelo.martindelacruz@gmail.com>
2025-09-30 11:04:05 +0800

    -debug some stuffs

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,27 +1,22 @@
-import './style.css'
-import { setupCounter } from './counter.js'
+// import './style.css'
+// //fix this where it should be working
+// let currentIndex = 0;
+// const slides = document.querySelectorAll("#carousel > div");
+// const totalSlides = slides.length;
+
+// function updateCarousel() {
+//   const offset = -currentIndex * 100;
+//   document.getElementById("carousel").style.transform = `translateX(${offset}%)`;
+// }
+
+// function nextSlide() {
+//   currentIndex = (currentIndex + 1) % totalSlides;
+//   updateCarousel();
+// }
+
+// function prevSlide() {
+//   currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+//   updateCarousel();
+// }
 
 
-
-let currentIndex = 0;
-const slides = document.querySelectorAll("#carousel > div");
-const totalSlides = slides.length;
-
-function updateCarousel() {
-  const offset = -currentIndex * 100;
-  document.getElementById("carousel").style.transform = `translateX(${offset}%)`;
-}
-
-function nextSlide() {
-  currentIndex = (currentIndex + 1) % totalSlides;
-  updateCarousel();
-}
-
-function prevSlide() {
-  currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
-  updateCarousel();
-}
-
-
-
-setupCounter(document.querySelector('#counter'))
